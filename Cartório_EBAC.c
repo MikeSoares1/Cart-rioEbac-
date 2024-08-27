ee24c3239230468a37033d6684fb39e38858848a
MikeSoares1 <miketestador@gmail.com>
2024-08-26 21:08:06 -0300

    Opção de saída
    Incluso a opção de saída para o usuário

--- Cartório_EBAC.c
+++ Cartório_EBAC.c
@@ -140,6 +140,7 @@ int main() //Função principal do projeto
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - consultar nomes\n");
     	printf("\t3 - Deletar nomes\n\n");
+    	printf("\t4 - Sair do sistema\n\n");
     	printf("Opção: "); //Fim do menu
 
         scanf("%d", &opcao); //Armazenando a esolha do usuário
@@ -160,6 +161,10 @@ int main() //Função principal do projeto
         	deletar();
     	    break;
     	    
+    	    case 4:
+    	    printf("Obrigado por utilizar o sistema!\n");
+    	    return 0;
+    	    break;
     	    
     	    
     	    default:
